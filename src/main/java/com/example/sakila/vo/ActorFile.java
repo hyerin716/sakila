46ab1bac6af5828fe3e585519a34ed3503225964
GD <GD@DESKTOP-N2TV7VQ>
2024-11-04 16:13:55 +0900

    ActorFile vo 추가

---
+++ src/main/java/com/example/sakila/vo/ActorFile.java
@@ -0,0 +1,16 @@
+package com.example.sakila.vo;
+
+import lombok.Data;
+
+@Data
+public class ActorFile {
+	private int actorFileId;	//PK
+	private int actorId;
+	private String originname;
+	private String filename;
+	private String ext;
+	private String type;
+	private long size;
+	private String createDate;
+	
+}
